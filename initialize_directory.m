2cb4b7c2a8e5e52cb77c3dcb249947e8eb19efc6
Vincent Bonnet <vincent.bonnet@u-pec.fr>
2022-04-28 00:53:30 +0200

    MATLAB 2017 compatibility bug, use char instead of string

--- initialize_directory.m
+++ initialize_directory.m
@@ -3,5 +3,5 @@ casadi_relative_path = '../casadi-windows-matlabR2016a-v3.5.5';
 addpath(casadi_relative_path);
 
 % Add all paths in this directory
-this_dir = "../Dumas_2";
+this_dir = '../Dumas_2';
 addpath(genpath(this_dir));
