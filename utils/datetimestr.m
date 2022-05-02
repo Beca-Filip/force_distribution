46554c2f665c48ee08a40c80e72dee5905df1f09
Beca-Filip <becaphilippe@gmail.com>
2022-05-02 14:10:17 +0200

    modified format because of windows

--- utils/datetimestr.m
+++ utils/datetimestr.m
@@ -2,6 +2,6 @@ function s = datetimestr()
 %DATETIMESTR returns the date and time in string format.
 
 dati = datetime;
-s = sprintf("%d/%02d/%02d-%02d:%02d:%02d", dati.Year, dati.Month, dati.Day, ...
-                                           dati.Hour, dati.Minute, round(dati.Second));
+s = sprintf("%d.%02d.%02d-%02d.%02d", dati.Year, dati.Month, dati.Day, ...
+                                           dati.Hour, dati.Minute);
 end
