456fa9a758cf5040f825ea93360a9f9038ba4e28
Filip <becaphilippe@gmail.com>
2022-05-30 11:23:38 +0200

    added lsq solution and displays to kkt analysi

--- testing/test_workspace_io_kktlsq/test_process_io_kkt_model_normalized_simulation_all_fun.m
+++ testing/test_workspace_io_kktlsq/test_process_io_kkt_model_normalized_simulation_all_fun.m
@@ -26,6 +26,7 @@ sample_list = 1:4:61;
 % Cost function parametrization
 % alpha = normalize_vector(rand(16, 1), 1);
 alpha = zeros(16, 1);
+alpha(16) = 1;
 
 % Perform the DO on these
 Fout = DO_subroutine_normalized(alpha, data, vars, model, sample_list, trial_list, speed_list, leg_list);
@@ -41,14 +42,19 @@ modelio = form_io_kktlsq_struct_normalized(data, model, vars, sample_list, trial
 % Process IO model
 modelio = process_io_kkt_model_normalized(modelio, -1e-1);
 
+% Solve using LSQ
+solio = solve_io_kkt_model_normalized(modelio);
+
 % Analyze IO model
 [~, ic_cf] = indexing_function(modelio, 'cf_parameters', [], [], [], []);
 min(svd(modelio.regressor(:, ic_cf)))
 min(svd(modelio.regressor))
 [u,s,v] = svd(modelio.regressor);
 
+
 solv.z = v(:, end);
 alpha.'
 [~, solv.alpha] = positive_negative_orthant_check(normalize_vector(v(1:length(alpha), end), 1));
 solv.alpha.'
+solio.alpha.'
 
