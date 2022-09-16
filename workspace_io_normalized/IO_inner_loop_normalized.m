15b57216ab2f2a3f1b0ec8e30016e0e095014a5a
Filip <becaphilippe@gmail.com>
2022-09-16 12:23:19 +0200

    Changed RMSE as the output of the inner loop to be a mean RMSE and not a total RMSE

--- workspace_io_normalized/IO_inner_loop_normalized.m
+++ workspace_io_normalized/IO_inner_loop_normalized.m
@@ -1,7 +1,7 @@
 function E = IO_inner_loop_normalized(alpha, data, vars, model, sample_list, trial_list, speed_list, leg_list)
 
 % Initialize error function
-E = 0;
+E = zeros(size(data.f, 1), length(trial_list), length(speed_list), length(leg_list));
 % Initialize total number of elemenst
 totnum = 0;
 
@@ -10,7 +10,17 @@ Fref = data.f(:,:,sample_list, trial_list, speed_list, leg_list);
 % Get prediction forces
 Fout = DO_subroutine_normalized(alpha, data, vars, model, sample_list, trial_list, speed_list, leg_list);
 
-% RMSE
-E = rmse(Fref, Fout);
-
+% Mean RMSE
+% For each demonstration
+for itr = 1 : length(trial_list)
+    for isp = 1 : length(speed_list)
+        for ile = 1 : length(leg_list)
+            % For each muscle
+            for mm = 1 : size(data.f, 1)                
+                E(mm, itr, isp, ile) = rmse(data.f(mm, :, :, trial_list(itr), speed_list(isp), leg_list(ile)), Fout(mm, :, :, itr, isp, ile));
+            end
+        end
+    end
+end
+E = mean(E(:));
 end
