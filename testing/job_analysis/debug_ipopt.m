d058603f95eb0b25148e696c0e243d6829986f7d
Beca-Filip <becaphilippe@gmail.com>
2022-10-27 15:24:32 +0200

    debug now displays scientific notation

--- testing/job_analysis/debug_ipopt.m
+++ testing/job_analysis/debug_ipopt.m
@@ -4,14 +4,14 @@ fprintf("-----------------------------------------------------------------\n");
 % Check function values
 debug_Jset = model.debug.value(vars.functions.Jset);
 fprintf("Objective function values:\n");
-fprintf("%.2f, ", debug_Jset);
+fprintf("%.2e, ", debug_Jset);
 fprintf("\n");
 
 % Check function gradient values
 debug_dJset = model.debug.value(jacobian(vars.functions.Jset, vars.variables.f)');
 
 fprintf("Objective function gradient norms:\n");
-fprintf("%.2f, ", sqrt(sum(debug_dJset.^2, 1)));
+fprintf("%.2e, ", sqrt(sum(debug_dJset.^2, 1)));
 fprintf("\n");
 
 % Check function hessian values
@@ -23,14 +23,14 @@ for ff = 1 : length(vars.functions.Jset)
 end
 
 fprintf("Objective function hessian determinants:\n");
-fprintf("%.2f, ", debug_detddJset);
+fprintf("%.2e, ", debug_detddJset);
 fprintf("\n");
 
 fprintf("-----------------------------------------------------------------\n");
 % Check inequality constraint values
 debug_c = model.debug.value(vars.functions.c);
 fprintf("Inequality constraint feasibility: %d \n", ~any(debug_c > 0));
-fprintf("Inequality constraint infeasibility: %.2f \n", max(debug_c));
+fprintf("Inequality constraint infeasibility: %.2e \n", max(debug_c));
 fprintf("Inequality constraint infeasible indices: \n")
 fprintf("%d, ", find(debug_c > 0));
 fprintf("\n");
@@ -39,12 +39,12 @@ fprintf("\n");
 debug_fmin = model.debug.value(vars.parameters.fmin);
 debug_fmax = model.debug.value(vars.parameters.fmax);
 fprintf("Inequality constraint bound gap: \n");
-fprintf("%.4f, ", debug_fmax - debug_fmin);
+fprintf("%.4e, ", debug_fmax - debug_fmin);
 fprintf("\n");
 
 fprintf("-----------------------------------------------------------------\n");
 % Check equality constraint values
 debug_ceq = model.debug.value(vars.functions.ceq);
-fprintf("Equality constraint infeasibility: %.2f \n", max(abs(debug_ceq)));
+fprintf("Equality constraint infeasibility: %.2e \n", max(abs(debug_ceq)));
 fprintf("\n");
 
